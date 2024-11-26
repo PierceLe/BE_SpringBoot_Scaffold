e4e0fa9f606eca194759c43abe7999afddbce752
PierceLe <haichau.hvsg@gmail.com>
2024-11-26 23:20:24 +1100

    feat: config database using jpa

---
+++ src/main/java/com/scaffold/spring_boot/entity/Users.java
@@ -0,0 +1,20 @@
+package com.scaffold.spring_boot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+}
