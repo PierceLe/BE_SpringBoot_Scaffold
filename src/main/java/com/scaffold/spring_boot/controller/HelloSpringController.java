0bd76137bd2132888ff9e813de521e0896c30696
PierceLe <haichau.hvsg@gmail.com>
2024-11-26 22:44:32 +1100

    chore: finish the scaffold for the project and test first API work

--- src/main/java/com/scaffold/spring_boot/controller/HelloSpringController.java
+++ src/main/java/com/scaffold/spring_boot/controller/HelloSpringController.java
@@ -1,2 +1,12 @@
-package com.scaffold.spring_boot.controller;public class HelloSpringController {
+package com.scaffold.spring_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringController {
+    @GetMapping("/hello")
+    String sayHello() {
+        return "Hello Spring Boot 3!";
+    }
 }
