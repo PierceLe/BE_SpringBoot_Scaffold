32d2adec56e6aadf931cd10926f640cf7bb48588
PierceLe <haichau.hvsg@gmail.com>
2024-12-07 02:56:45 +0700

    chore: do the same for username

--- src/main/java/com/scaffold/spring_boot/dto/request/UserCreationRequest.java
+++ src/main/java/com/scaffold/spring_boot/dto/request/UserCreationRequest.java
@@ -7,11 +7,13 @@ import java.time.LocalDate;
 
 @Data
 public class UserCreationRequest {
+    @Size(min = 3, message = "username must be at least 3 characters")
     private String username;
 
     @Size(min = 8, message = "password must be at least 8 characters")
     private String password;
     private String firstName;
     private String lastName;
+
     private LocalDate dob;
 }
