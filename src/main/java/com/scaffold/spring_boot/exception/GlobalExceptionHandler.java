9fe3ee684dbadd477db22e7654193ff4d54c3c60
PierceLe <haichau.hvsg@gmail.com>
2024-12-07 02:54:28 +0700

    chore: added global exception for MethodArgumentNotValid Exception

--- src/main/java/com/scaffold/spring_boot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/scaffold/spring_boot/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.scaffold.spring_boot.exception;
 
+import jakarta.validation.ValidationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,6 +11,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<String> handlingRuntimeException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<String> handlingValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(e.getFieldError().getDefaultMessage());
     }
 
 
