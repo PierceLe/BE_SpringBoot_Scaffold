e3bebf9ab0b81934d45b95135fc90055fb9f7242
PierceLe <haichau.hvsg@gmail.com>
2024-12-07 02:47:35 +0700

    feat: example for validation for user creation password

--- src/main/java/com/scaffold/spring_boot/dto/request/UserCreationRequest.java
+++ src/main/java/com/scaffold/spring_boot/dto/request/UserCreationRequest.java
@@ -1,5 +1,6 @@
 package com.scaffold.spring_boot.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,6 +8,8 @@ import java.time.LocalDate;
 @Data
 public class UserCreationRequest {
     private String username;
+
+    @Size(min = 8, message = "password must be at least 8 characters")
     private String password;
     private String firstName;
     private String lastName;
