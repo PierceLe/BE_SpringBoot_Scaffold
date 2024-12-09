8839028d7fa2e65a98f7a34a8883d5a62c351f70
PierceLe <haichau.hvsg@gmail.com>
2024-12-09 10:00:17 +0700

    feat: implemented same with other classes

--- src/main/java/com/scaffold/spring_boot/dto/request/ApiResponse.java
+++ src/main/java/com/scaffold/spring_boot/dto/request/ApiResponse.java
@@ -1,10 +1,15 @@
 package com.scaffold.spring_boot.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 // null attribute will not being shown
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
