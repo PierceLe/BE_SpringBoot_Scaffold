331f27f8b1dbc17954fd438aaa37b5faa45a9d6a
PierceLe <haichau.hvsg@gmail.com>
2024-12-09 15:36:58 +0700

    feat: finish new api for checking the validity of jwt

--- src/main/java/com/scaffold/spring_boot/controller/AuthenticationController.java
+++ src/main/java/com/scaffold/spring_boot/controller/AuthenticationController.java
@@ -6,10 +6,7 @@ import com.scaffold.spring_boot.dto.response.AuthenticationResponse;
 import com.scaffold.spring_boot.service.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -17,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public ApiResponse<AuthenticationResponse> authenticate(@RequestBody @Valid AuthenticationRequest authenticationRequest) {
         return ApiResponse.<AuthenticationResponse>builder().
                 result(authenticationService.authenticateUser(authenticationRequest))
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    public ApiResponse<AuthenticationResponse> authenticateJwt(@RequestHeader("Authorization") @Valid AuthenticationRequest authenticationRequest) {
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(authenticationService.authenticateUser(authenticationRequest))
                 .build();
     }
 }
