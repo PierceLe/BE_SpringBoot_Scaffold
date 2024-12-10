fc80989a9d8763f7485eb84fffe4c9db6ea95d49
PierceLe <haichau.hvsg@gmail.com>
2024-12-10 09:59:19 +0700

    chore: config spring security

---
+++ src/main/java/com/scaffold/spring_boot/configuration/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.scaffold.spring_boot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+}
